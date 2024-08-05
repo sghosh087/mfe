83312a24a6dac0d14f012e23217342af21d5281d
Soumya Ghosh <soumya@soumya-HP-Notebook>
2024-08-05 20:40:16 +0530

    Created container deploy script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,32 +1,44 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
 
-module.exports = merge(common, {
+const domain = process.env.PRODUCTION_DOMAIN;
+
+module.exports = merge(commonConfig, {
     mode: "production",
     output: {
-        filename: '[name][contenthash:12].js'
+        filename: '[name].[contenthash:12].js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                exclude: /\.module\.css/,
-                use: ['style-loader', 'css-loader']
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.css$/,
+    //             exclude: /\.module\.css/,
+    //             use: ['style-loader', 'css-loader']
+    //         },
+    //         {
+    //             test: /\.css$/,
+    //             include: /\.module\.css/,
+    //             use: ['style-loader', {
+    //                 loader: 'css-loader',
+    //                 options: {
+    //                     module: {
+    //                         localIdentName: '[path][name]__[local]--[hash:base64:5]'
+    //                     }
+    //                 }
+    //             }]
+    //         }
+    //     ]
+    // },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`
             },
-            {
-                test: /\.css$/,
-                include: /\.module\.css/,
-                use: ['style-loader', {
-                    loader: 'css-loader',
-                    options: {
-                        module: {
-                            localIdentName: '[path][name]__[local]--[hash:base64:5]'
-                        }
-                    }
-                }]
-            }
-        ]
-    }
-})
+            shared: packageJson.dependencies
+        }),
 
-module.exports = common
+    ]
+})
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,32 +1,42 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
 
-module.exports = merge(common, {
+module.exports = merge(commonConfig, {
     mode: "production",
     output: {
-        filename: '[name][contenthash:12].js'
+        filename: '[name].[contenthash:12].js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                exclude: /\.module\.css/,
-                use: ['style-loader', 'css-loader']
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.css$/,
+    //             exclude: /\.module\.css/,
+    //             use: ['style-loader', 'css-loader']
+    //         },
+    //         {
+    //             test: /\.css$/,
+    //             include: /\.module\.css/,
+    //             use: ['style-loader', {
+    //                 loader: 'css-loader',
+    //                 options: {
+    //                     module: {
+    //                         localIdentName: '[path][name]__[local]--[hash:base64:5]'
+    //                     }
+    //                 }
+    //             }]
+    //         }
+    //     ]
+    // }
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
             },
-            {
-                test: /\.css$/,
-                include: /\.module\.css/,
-                use: ['style-loader', {
-                    loader: 'css-loader',
-                    options: {
-                        module: {
-                            localIdentName: '[path][name]__[local]--[hash:base64:5]'
-                        }
-                    }
-                }]
-            }
-        ]
-    }
+            shared: packageJson.dependencies
+        })
+    ]
 })
-
-module.exports = common
