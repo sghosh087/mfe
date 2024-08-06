bc814b16878e91d8ede950dfeef4693c416abc6f
Soumya Ghosh <soumya@soumya-HP-Notebook>
2024-08-06 16:43:55 +0530

    randomize profuction class names

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 const App = () => {
-    return <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
-    </BrowserRouter>
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </BrowserRouter >
+    )
 }
 export default App;
